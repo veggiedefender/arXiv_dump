623e7cd29643ce33af5f9b42cb487dc03afed93f
Jesse Li <jessejesse123@gmail.com>
2017-07-04 10:42:32 -0400

    download files and extract into out/

--- get_arxiv_data.sh
+++ get_arxiv_data.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/bash
 
 s3cmd get 's3://arxiv/src/arXiv_src_manifest.xml' --requester-pays
-filenames=($(grep -oP 'src\/.+\.tar' arXiv_src_manifest.xml))
+mkdir -p out
+
+filenames=($(grep -oP 'arXiv_.*\.tar' arXiv_src_manifest.xml))
 
 for filename in "${filenames[@]}"
 do
     echo "$filename"
+    s3cmd get "s3://arxiv/src/$filename" out --requester-pays
+    cd out
+
+    tar -xf "$filename"
+    rm "$filename"
+
+    cd ..
 done
