a43ad532085e44dd8be2e96e64f6740e1d6aa7bf
Jesse Li <jessejesse123@gmail.com>
2017-07-04 16:49:01 -0400

    fix issues found by shellcheck

--- get_arxiv_data.sh
+++ get_arxiv_data.sh
@@ -2,40 +2,35 @@
 
 s3cmd get 's3://arxiv/src/arXiv_src_manifest.xml' --requester-pays
 mkdir -p out
-
-filenames=($(grep -oP 'arXiv_.*\.tar' arXiv_src_manifest.xml))
+mapfile -t filenames < <(grep -oP 'arXiv_.*\.tar' arXiv_src_manifest.xml)
 
 for filename in "${filenames[@]}"
 do
     echo "$filename"
     s3cmd get "s3://arxiv/src/$filename" out --requester-pays
-    cd out
+    cd out || exit
 
     tar -xf "$filename"
     rm "$filename"
-    cd $(echo "$filename" | cut -d '_' -f 3)
+    cd "$(echo "$filename" | cut -d '_' -f 3)" || exit
 
-    find -maxdepth 1 -type f -not -name "*.gz" -delete
+    find . -maxdepth 1 -type f -not -name "*.gz" -delete
 
-    papers=($(ls *.gz))
+    mapfile -t papers < <(ls -- *.gz)
     for paper in "${papers[@]}"
     do
         folder=$(echo "$paper" | cut -d '.' -f 1)
         mkdir "$folder"
         mv "$paper" "$folder"
-        cd "$folder"
+        cd "$folder" || exit
 
-        tar -xf "$paper" 2>/dev/null
-        if [ $? -eq 0 ]; then
-            echo "Extracted $paper with tar -xf"
+        if tar -xf "$paper" 2>/dev/null
+        then
             rm "$paper"
-        else
-            echo "Failed extracting $paper with tar -xf"
-            gunzip "$paper" 2>/dev/null
-            if [ $? -eq 0 ]; then
-                echo "EXTRACTED $paper with gunzip"
-            else
-                echo "Failed extracting $paper with gunzip"
+        else          
+            if ! gunzip "$paper" 2>/dev/null
+            then
+                echo "FAILED EXTRACTING $paper WITH BOTH tar -xf AND gunzip!"
                 rm "$paper"
             fi
         fi
